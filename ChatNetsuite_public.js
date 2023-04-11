6c3049d42a96d75219db3a0e61221b327e506cf2
karthous <54101610+karthous@users.noreply.github.com>
2023-04-11 11:58:03 +0800

    v0.010

---
+++ ChatNetsuite_public.js
@@ -0,0 +1,131 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ * @author Jiyun Mei
+ * @email karthous@outlook.com
+ * @since 2023.4
+ */
+define(['N/https', 'N/ui/serverWidget'],
+    (https, serverWidget) => {
+
+        const VERSION_NO = '0.01';
+        // ENTER YOUR SCRIPT URL HERE
+        const SCRIPT_URL = 'https://1234567.app.netsuite.com/app/site/hosting/scriptlet.nl?script=1234&deploy=1';
+        // ENTER YOUR API KEY HERE
+        const OPENAI_KEY = 'sk-123456789012345678901234567890123456789012345678';
+        const MODEL = 'text-davinci-003';
+        const MAX_TOKENS = 500;
+
+        /**
+         * Handles the request when a user visits the page.
+         * If the request method is GET, the initPage will be rendered.
+         * Otherwise, the answerPage is rendered.
+         * @param {Object} scriptContext
+         */
+        const onRequest = (scriptContext) => {
+            if (scriptContext.request.method === 'GET') {
+                scriptContext.response.writePage({pageObject: initPage()});
+            } else {
+                scriptContext.response.writePage({pageObject: answerPage(scriptContext)});
+            }
+        }
+
+        /**
+         * Creates an HTML form with two fields, a text input and a submit button.
+         * The form is initialized with the title 'ChatNetsuite v[VERSION_NO]'.
+         * The text input is labeled 'Input' and the submit button is labeled 'Ask'.
+         */
+        const initPage = () => {
+            let form = serverWidget.createForm({title: 'ChatNetsuite v' + VERSION_NO});
+            form.addField({id:'custpage_input', label:'Input', type: serverWidget.FieldType.LONGTEXT});
+            form.addSubmitButton({label: 'Ask'});
+            return form;
+        }
+
+        /**
+         * Renders the answer page with the answer to the customer's query.
+         * It first retrieves the input text from the request parameter and
+         * makes an API call to OpenAI using the provided key and model to get the answer.
+         * The answer is parsed from the response body and a form with the input and answer is created.
+         * The form is then returned to the user.
+         * @param {Object} scriptContext
+         */
+        const answerPage = (scriptContext) => {
+            let text = scriptContext.request.parameters.custpage_input;
+            log.debug("-> text", text);
+            // Set API URL, headers and payload
+            const apiUrl = 'https://api.openai.com/v1/completions';
+            const headers = {
+                'Content-Type': 'application/json; charset=utf-8',
+                'Authorization': 'Bearer ' + OPENAI_KEY
+            };
+            let payload = {
+                "model": MODEL,
+                "prompt": text,
+                "max_tokens": MAX_TOKENS
+            };
+            // Make API call
+            let response = https.post({
+                url: apiUrl,
+                body: JSON.stringify(payload),
+                headers: headers
+            });
+            log.debug('response body', JSON.parse(response.body));
+            // Handle response codes
+            let answer;
+            switch (response.code) {
+                case 200:
+                    answer = JSON.parse(response.body).choices[0].text;
+                    log.debug('answer', answer);
+                    break;
+                case 401:
+                    answer = 'Incorrect API key provided';
+                    break;
+                case 402:
+                    answer = 'Server refused to access, please try again later';
+                    break;
+                case 502:
+                    answer = 'Bad Gateway';
+                    break;
+                case 503:
+                    answer = 'Server is busy, please try again later';
+                    break;
+                case 504:
+                    answer = 'Gateway Time-out';
+                    break;
+                case 500:
+                    answer = 'Internal Server Error';
+                    break;
+                default:
+                    answer = 'Unexpected Error';
+            }
+            // Create form
+            let form = serverWidget.createForm({
+                title: 'ChatNetsuite v' + VERSION_NO
+            });
+            let fieldGroup = form.addFieldGroup({id:'custpage_fieldgroup', label:' '});
+            fieldGroup.isSingleColumn = true;
+            let inputText = form.addField({
+                id: 'custpage_input',
+                type: serverWidget.FieldType.INLINEHTML,
+                label: 'You',
+                container: 'custpage_fieldgroup'
+            });
+            inputText.defaultValue = '<br><h2 style="color:#607799;">You:</h2>' +
+                '<article style="font-size:medium;">' + text + '</article>';
+            let answerText = form.addField({
+                id: 'custpage_answertext',
+                type: serverWidget.FieldType.INLINEHTML,
+                label: 'answer',
+                container: 'custpage_fieldgroup'
+            });
+            answerText.defaultValue = '<br>' +
+                '<h2 style="color:#607799;">ChatNetsuite:</h2>' +
+                '<article style="font-size:medium;">' + answer + '</article>';
+            answerText.defaultValue += '<br><br>' +
+                '<a href="' + SCRIPT_URL + '" style="color:#607799;">Back</a>';
+            return form;
+        }
+
+        return {onRequest}
+    });
---
+++ README.md
@@ -0,0 +1,50 @@
+![image](ChatNetsuite_logo.png)
+
+# ChatNetsuite 0.0.1
+
+A basic chat interface deployed in Netsuite, using OpenAI&#39;s API.
+
+## Table of Contents
+
+- [Usage](#usage)
+- [Maintainers](#maintainers)
+- [Contributing](#contributing)
+- [Disclaimer](#disclaimer)
+
+## Usage
+
+To use ChatNetsuite in NetSuite, you can follow these steps:
+
+1. Upload chatNetsuite.js to SuiteScripts folder in the File Cabinet.
+2. Go to Customization > Scripts > New.
+3. Select ChatNetsuite_public.js and click on 'Create Script Record'.
+4. Set the name of the script and click on 'Save'.
+5. Click on 'Deploy Script'.
+6. Set script deployment configuration and click on 'Save'
+7. Click on the url under the field 'URL'.
+8. Configure OPENAI_KEY and SCRIPT_URL in ChatNetsuite_public.js file.
+9. Alternatively, click on 'Edit' and configure 'Links' to place ChatNetsuite on the navigation bar.
+10. Enjoy.
+
+You can refer to NetSuite's SuiteScript documentation for more detailed information.
+
+## Maintainers
+
+[@karthous](https://github.com/karthous)
+
+## Contributing
+
+PRs accepted.
+
+## Disclaimer
+
+This is not an official product of Oracle, Netsuite, or OpenAI. 
+The authors of this project are not responsible for any content generated using this interface.
+By using this software, you agree not to:
+
+1. Violates any laws.
+2. Produces any harm to a person or persons.
+3. Disseminates (spreads) any personal information that would be meant for harm.
+4. Spreads misinformation.
+5. Target vulnerable groups.
+
